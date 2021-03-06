7b3ee4938610bcb7a3b4867058b2cdbc425a4095
Desley Aalderink <>
2021-03-06 15:00:06 +0100

    forgot to save

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,18 @@
 require("./bootstrap.js");
 
-<<<<<<< HEAD
 import Vue from "vue";
 import VueRouter from "vue-router";
-=======
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import '../css/app.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "../css/app.css";
 
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
->>>>>>> develop
+Vue.use(IconsPlugin);
 
 //Main pages
 import App from "./views/App.vue";
